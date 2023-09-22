486e0b07eeba8d98774d54e0d844710773f9d4f9
MatheusSQueiroz <matheussq15@gmail.com>
2023-09-22 16:36:38 -0300

    Criando Get

---
+++ blogpessoal/Validator/PostagemValidator.cs
@@ -0,0 +1,22 @@
+﻿using blogpessoal.Model;
+using FluentValidation;
+
+namespace blogpessoal.Validator
+{
+    public class PostagemValidator : AbstractValidator<Postagem>
+    {
+        public PostagemValidator() 
+        {
+            //Regras de validação
+            RuleFor(p => p.Titulo)
+                .NotEmpty()
+                .MinimumLength(5)
+                .MaximumLength(100);
+
+            RuleFor(p => p.Texto)
+                .NotEmpty()
+                .MinimumLength(10)
+                .MaximumLength(1000);
+        }
+    }
+}
